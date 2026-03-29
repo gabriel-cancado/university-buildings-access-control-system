2cf6aa64ad48f35db9473af5101e2960688bf36d
gabriel.cancado <gabriel.cancado@gmail.com>
2026-03-29 19:43:37 -0300

    adding common.h

--- include/common.h
+++ include/common.h
@@ -1,3 +1,24 @@
 #pragma once
 
-void usage_exit(int argc, char** argv);
+#define MAX_USERS 30;
+#define MAX_CLIENTS 10;
+#define MAX_PEERS 1;
+
+typedef struct {
+    char uid[11];
+    int is_special;
+} user_auth;
+
+typedef struct {
+    char uid[11];
+    int last_location;
+} user_loc;
+
+void error_exit(char* msg);
+
+void log_exit(char* msg);
+
+/*
+* Parses a string to internet address (IPv4 or IPv6)
+*/
+int addr_parse(char* addr_str, char* port_str, struct sockaddr_storage *storage);
