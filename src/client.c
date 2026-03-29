01a405f8bd85598b4a58e96c1a0aa88af441888b
gabriel.cancado <gabriel.cancado@gmail.com>
2026-03-29 19:39:45 -0300

    Creating addr_parse logic

---
+++ src/client.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "../include/common.h"
+
+void usage_exit(int argc, char** argv) {
+    char* progam_name = argv[0];
+    printf("usage: %s <servers IP> <users server port> <loc server port> <loc_id>\n", progam_name);
+    printf("example: %s 127.0.0.1 50000 6000 2\n", progam_name);
+
+    exit(EXIT_FAILURE);
+}
+
+int create_socket(struct sockaddr_storage* storage) {
+    int is_ipv4 = storage->ss_family == AF_INET;
+}
+
+int openConnection(char* server_addr_str, char* server_port_str) {
+    struct sockaddr_storage storage;
+    int success = addr_parse(server_addr_str, server_port_str, &storage) == 0;
+    if (!success) log_exit("Invalid server address. Valid types are IPv4 and IPv6\n");
+
+    int s = socket(storage.ss_family, SOCK_STREAM, 0);
+    if (s == -1) log_exit("Error creating socket");
+}
+
+void main (int argc, char** argv) {
+    if (argc < 5) usage_exit(argc, argv);
+
+    int loc_id = atoi(argv[4]);
+    if (loc_id < 0 || loc_id > 10) error_exit("Invalid argument");
+
+    int users_server_socket = openConnection(argv[1], argv[2]);
+}
--- src/common.c
+++ src/common.c
@@ -1,15 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
 
-void usage_exit(int argc, char** argv) {
-    char* progam_name = argv[0];
-    printf("usage: %s <server IP> <server port>\n", progam_name);
-    printf("example: %s 127.0.0.1 5000\n", progam_name);
-
+void error_exit(char* msg) {
+    printf("%s\n", msg);
     exit(EXIT_FAILURE);
 }
 
 void log_exit(char* msg) {
     perror(msg);
     exit(EXIT_FAILURE);
+}
+
+int addr_parse(char* addr_str, char* port_str, struct sockaddr_storage *storage) {
+    if (addr_str == NULL || port_str == NULL) return -1;
+
+    uint16_t port = (uint16_t)atoi(port_str); // port numbers are 16 bits
+    if (port == 0) return -1;
+
+    port = htons(port); // host to network short
+
+    // We first try to parse the address to IPv4, if it doesn`t work we try IPv6
+    struct in_addr in_addr_ipv4; // 32-bit IP address
+    int is_ipv4_addr = inet_pton(AF_INET, addr_str, &in_addr_ipv4);
+    if (is_ipv4_addr) {
+        struct sockaddr_in *addr_ipv4 = (struct sockaddr_in *)storage;
+        *addr_ipv4 = (struct sockaddr_in){ .sin_family = AF_INET, .sin_addr = in_addr_ipv4, .sin_port = port };
+        return 0;
+    }
+
+    struct in6_addr in_addr_ipv6; // 128-bit IPv6 address
+    int is_ipv6_addr = inet_pton(AF_INET6, addr_str, &in_addr_ipv6);
+    if (is_ipv6_addr) {
+        struct sockaddr_in6 *addr_ipv6 = (struct sockaddr_in6 *)storage;
+        *addr_ipv6 = (struct sockaddr_in6){ .sin6_family = AF_INET6, .sin6_port = port };
+        memcpy(&(addr_ipv6->sin6_addr), &in_addr_ipv6, sizeof(in_addr_ipv6));
+        return 0;
+    }
+
+    return -1;
 }
